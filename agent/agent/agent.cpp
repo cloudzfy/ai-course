c923346880e756b020cd9c6d01cca4d54dea8841
Youzheng Chen <cloudzfy@Cloudzfys-MacBook-Pro.local>
2014-09-30 14:45:52 -0700

    Start Homework 2

---
+++ agent/agent/agent.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+char map[10][10];
+
+int main(int argc, const char * argv[]) {
+    int task_no, depth;
+    string player;
+    FILE *in, *out;
+    in = freopen("input.txt", "r", stdin);
+    out = freopen("output.txt", "w", stdout);
+    cin>>task_no>>player>>depth;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            cin>>map[i][j];
+        }
+    }
+    fclose(in);
+    fclose(out);
+    return 0;
+}
